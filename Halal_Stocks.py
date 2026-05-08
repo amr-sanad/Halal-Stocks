d4a45dfe7f042c2f9c584443e56a0d9bc62b59f2
amr-sanad <amrosanad@gmail.com>
2026-05-08 08:51:00 +0200

    Csv portfolio Halal_Stocks.py

--- Halal_Stocks.py
+++ Halal_Stocks.py
@@ -22,7 +22,7 @@ st.info(
 # --------------------------------------------------
 # LOAD PORTFOLIO
 # --------------------------------------------------
-portfolio_df = pd.read_excel("portfolio.xlsx")
+portfolio_df = pd.read_csv("portfolio.csv")
 tickers = portfolio_df["Ticker"].dropna().str.upper().tolist()
 
 # --------------------------------------------------
